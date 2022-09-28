771a07c2bfa1b043d60c0343ee470c9f785c38b6
elizavetacher44@gmail.com <elizavetacher44@gmail.com>
2022-09-28 11:50:53 +1000

    Изменения 1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,37 @@ namespace ISRPO_PR4_Cherednichenko
         {
             InitializeComponent();
         }
+
+
+        private void ButtonSbros_Click(object sender, RoutedEventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+        }
+
+        private void Button1Pl3_Click(object sender, RoutedEventArgs e)
+        {
+            double a = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = textBox1.Text + 3;
+        }
+
+        private void Button1Pl2_Click(object sender, RoutedEventArgs e)
+        {
+            double a = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = textBox1.Text + 2;
+        }
+
+        private void Button1Pl1_Click(object sender, RoutedEventArgs e)
+        {
+            double a = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = textBox1.Text + 1;
+        }
+
+        private void Button1M1_Click(object sender, RoutedEventArgs e)
+        {
+            double a = Convert.ToDouble(textBox1.Text);
+            a = a - 1;
+        }
     }
 }
