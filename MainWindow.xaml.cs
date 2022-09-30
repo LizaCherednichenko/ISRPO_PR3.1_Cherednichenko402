404bd9fba57c669d1ecf9d21adbd448c0f1c76ea
elizavetacher44@gmail.com <elizavetacher44@gmail.com>
2022-10-01 09:36:03 +1000

    готовое

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,35 +26,92 @@ namespace ISRPO_PR4_Cherednichenko
         }
 
 
+        float a, b;
+        int count;
+
+
+
         private void ButtonSbros_Click(object sender, RoutedEventArgs e)
         {
-            textBox1.Clear();
-            textBox2.Clear();
-            textBox3.Clear();
+            textBox1.Text = "0";
+            textBox2.Text = "0";
+            textBox3.Text = "0";
         }
 
         private void Button1Pl3_Click(object sender, RoutedEventArgs e)
         {
-            double a = Convert.ToDouble(textBox1.Text);
-            textBox1.Text = textBox1.Text + 3;
+            a = float.Parse(textBox1.Text);
+            count = 1;
+            calculate();
         }
 
         private void Button1Pl2_Click(object sender, RoutedEventArgs e)
         {
-            double a = Convert.ToDouble(textBox1.Text);
-            textBox1.Text = textBox1.Text + 2;
+            a = float.Parse(textBox1.Text);
+            count = 2;
+            calculate();
         }
 
         private void Button1Pl1_Click(object sender, RoutedEventArgs e)
         {
-            double a = Convert.ToDouble(textBox1.Text);
-            textBox1.Text = textBox1.Text + 1;
+            a = float.Parse(textBox1.Text);
+            count = 3;
+            calculate();
         }
 
         private void Button1M1_Click(object sender, RoutedEventArgs e)
         {
-            double a = Convert.ToDouble(textBox1.Text);
-            a = a - 1;
+            a = float.Parse(textBox1.Text);
+            count = 4;
+            calculate();
+        }
+
+        private void Button1M2_Click(object sender, RoutedEventArgs e)
+        {
+            a = float.Parse(textBox1.Text);
+            count = 5;
+            calculate();
+        }
+
+        private void Button1M3_Click(object sender, RoutedEventArgs e)
+        {
+            a = float.Parse(textBox1.Text);
+            count = 6;
+            calculate();
+        }
+
+        private void calculate()
+        {
+            
+            switch (count)
+            {
+                case 1:
+                    b = a + 3;
+                    textBox1.Text = b.ToString();
+                    break;
+                case 2:
+                    b = a + 2;
+                    textBox1.Text = b.ToString();
+                    break;
+                case 3:
+                    b = a + 1;
+                    textBox1.Text = b.ToString();
+                    break;
+                case 4:
+                    b = a - 1;
+                    textBox1.Text = b.ToString();
+                    break;
+                case 5:
+                    b = a - 2;
+                    textBox1.Text = b.ToString();
+                    break;
+                case 6:
+                    b = a - 3;
+                    textBox1.Text = b.ToString();
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
